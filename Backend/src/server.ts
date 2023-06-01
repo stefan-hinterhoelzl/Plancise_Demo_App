f3bfadff31da9d0f4a0a9c44b650a7fff2dd1207
Stefan Hinterhölzl <hinterhoelzlstefan@gmail.com>
2023-06-01 22:11:51 +0200

    initial setup begun

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -1,17 +1,16 @@
-import dotenv from 'dotenv';
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
+import dotenv from "dotenv";
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
 
 //Initialization
 const app = express();
 dotenv.config();
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(express.json());
 
-
 //CORS
-const allowedOrigins = ['http://localhost:4200'];
+const allowedOrigins = ["http://localhost:4200"];
 const options: cors.CorsOptions = {
   origin: allowedOrigins,
 };
@@ -19,18 +18,13 @@ const options: cors.CorsOptions = {
 app.use(cors(options));
 
 
-
-
-
-
-
-
-
+app.get('/', (req, res) => {
+  res.json({'Message':'Works'})
+})
 
 
 
 //Start the Server
 const server = app.listen(process.env.PORT, () => {
-    console.log('Backend Server started on Port ' + process.env.PORT);
-  });
-  
+  console.log("Backend Server started on Port " + process.env.PORT);
+});
