695c439bcfa302078502f180d1981182ce1fb6dd
Stefan Hinterhölzl <hinterhoelzlstefan@gmail.com>
2023-06-07 23:38:23 +0200

    login?

--- Frontend/src/app/login/login.component.ts
+++ Frontend/src/app/login/login.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+
+  auth = inject(AuthService)
+
+
+
+  login() {
+    this.auth.socialLogin()
+  }
 
 }
--- Frontend/src/app/services/auth.service.ts
+++ Frontend/src/app/services/auth.service.ts
@@ -1,9 +1,54 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { SnackbarComponent } from '../shared/snackbar/snackbar.component';
+import { Router } from '@angular/router';
+import {
+  GoogleAuthProvider,
+  getAuth,
+  getRedirectResult,
+  signInWithRedirect,
+  signOut,
+} from 'firebase/auth';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor() {}
 
-  constructor() { }
+  router = inject(Router);
+  snackbar = inject(SnackbarComponent);
+
+  async socialLogin() {
+    const auth = getAuth();
+
+    let provider = new GoogleAuthProvider();
+
+    await signInWithRedirect(auth, provider);
+
+    const result = await getRedirectResult(auth);
+    console.log(result)
+    if (result) {
+      console.log(result)
+      const user = result.user;
+      this.snackbar.openSnackBar('Logged in!', 'green-snackbar');
+      this.router.navigate([' ']);
+    } else {
+      this.snackbar.openSnackBar('Login failed!', 'red-snackbar');
+    }
+  }
+
+  logout() {
+    const auth = getAuth();
+    signOut(auth)
+      .then(() => {
+        this.snackbar.openSnackBar('Logged out!', 'green-snackbar');
+        this.router.navigate(['/login']);
+      })
+      .catch((error) => {
+        this.snackbar.openSnackBar(
+          'Logout failed, try again later!',
+          'red-snackbar'
+        );
+      });
+  }
 }
